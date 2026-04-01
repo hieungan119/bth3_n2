b87caaf89d3cec5eaf59c6107ebdae3304e200f4
hieungan119 <030239230139@st.buh.edu.vn>
2026-04-01 09:07:14 +0700

    ngan

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+use Illuminate\Support\Facades\Schema; // 1. Thêm dòng này vào
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+
+        //
+        Schema::defaultStringLength(191); // 2. Thêm dòng này vào
+
+    }
+}
